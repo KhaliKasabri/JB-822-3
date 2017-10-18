ec172cb28aba7faa90c89167173d22798326254e
KhaliKasabri <32670821+KhaliKasabri@users.noreply.github.com>
2017-10-18 12:14:05 +0300

    RandomUnique
    Hi Amid,
    I'm stuck with this for hours now! 3rd assignment, I just want to pick a random number which all digits are unique..
    I'm frustrated really with it!

--- src/ameedf/jb8223/assignments/GuessTheNumber.java
+++ src/ameedf/jb8223/assignments/GuessTheNumber.java
@@ -1,26 +1,33 @@
-package ameedf.jb8223.assignments;
+import java.util.Random;
+import java.util.Arrays;
 
-/**
- * Your assignment is to create the game "Guess The Number". Here's a description of the game:
- * (Taken from "http://www.goobix.com/games/guess-the-number/")
- * Guess The Number is a game where you must use your logic in order to guess a 4-digit secret number
- * selected by the computer at the beginning of the game. The number is formed with digits from 0 to 9;
- * each digit appears once at most. This number is guessed by you via multiple attempts. An attempt
- * consists in a proposed number, selected by you, and the computer's reply. The computer must tell you,
- * in his reply, how many digits have you guessed on the same position, and how many digits have you
- * guessed on a different position. Using information from the computer's reply, you must guess the
- * number using as few moves as possible.
- * <p>
- * 1. Your implementation must be done using ONLY thing we've learned
- * 2. BEFORE entering a new guess by the user, the program will show the previous guesses and their results
- *
- * BONUS: add some more statistics about the users:
- * At the beginning of a new game, the user is asked to enter his unique number (from 1 to 100)
- * The user is given the chance to enter the number of digits to guess (not only 4)
- * When the user finally guesses the digits, the number of steps will be updated in the high-scores table
- * The user will be told about his position in the high-scores table at the end of his game (for example,
- * "There are X players with better scores") or if he's at the top of the table.
- * Before the next game, the top 3 scores (which are greater than 0) will be printed
- */
-public class GuessTheNumber {
+public class RndUnq {
+	public static void main(String[] args) {
+		int[] ar = new int[10];
+		Random rnd = new Random();
+
+		ar[0] = rnd.nextInt(10);
+		int c = 1;
+    // c is a counter to use in compairing the numbers in an array
+    // i is an index for the cells in the array
+    
+		for (int i = 1; i < 10; i++) {
+
+			ar[i] = rnd.nextInt(10);
+			
+			while (c <= i) {
+
+				if (ar[i] != ar[i - c]) {
+					c++;
+				} else {
+					ar[i] = rnd.nextInt(10);
+					continue;
+
+				}
+			}
+		}
+		System.out.println(Arrays.toString(ar));
+
+	}
+
 }
